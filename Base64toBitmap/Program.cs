d1233d84619798ca867941397e2c16c50c562d31
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-04-25 16:22:56 +0700

    FInal

--- Base64toBitmap/Program.cs
+++ Base64toBitmap/Program.cs
@@ -4,6 +4,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Base64toBitmap
 {
@@ -18,15 +20,10 @@ namespace Base64toBitmap
 
             DirectoryInfo d = new DirectoryInfo(txtFolder);//Assuming Test is your Folder
             List<FileInfo> Files = d.GetFiles("*.txt")
-                                    .OrderBy(f => f.Length)
-                                    .ToList(); //Getting Text files
-
-            //fileNames = fileNames.OrderBy(a => Guid.NewGuid()).ToList();
-            //fileNames.Sort();
+                                    //.OrderBy(f => f.Length)
+                                    .ToList();
 
             int widthPx = 0;
-            int sum = 400;
-            int koef = -1;
             foreach (FileInfo file in Files)
             {
                 try
@@ -34,21 +31,30 @@ namespace Base64toBitmap
                     string path = txtFolder + file.Name;
                     string ImageText = File.ReadAllText(path);
                     Byte[] bitmapData = Convert.FromBase64String(FixBase64ForImage(ImageText));
+                    int position = 0;
+
+                    string decodedString = Encoding.UTF8.GetString(bitmapData);
+                    Regex regex = new Regex(@"my position b['].+[']");
+                    Match match = regex.Match(decodedString);
+                    if (match.Success)
+                    {
+                        Match matchInBrackets = new Regex(@"['].+[']").Match(match.Value);
+
+                        string posEncoded = new Regex(@"[a-zA-Z0-9]+").Match(matchInBrackets.Value).Value;
+                        string posDecoded = Encoding.UTF8.GetString(Convert.FromBase64String(posEncoded));
+                        position = int.Parse(new Regex(@"[0-9]+").Match(posDecoded).Value);
+                    }
+
                     MemoryStream streamBitmap = new MemoryStream(bitmapData);
                     Bitmap bitImage = new Bitmap((Bitmap)Image.FromStream(streamBitmap));
 
                     Graphics gr = Graphics.FromImage(finalBitmap);
 
-                    var pos = widthPx;
-                    //pos = 400 + koef * sum + -koef * widthPx;
-
-                    gr.DrawImage(bitImage, pos, 0);
-                    koef *= -1;
-                    widthPx++;
+                    gr.DrawImage(bitImage, position, 0);
                 }
                 catch
                 {
-                    Console.WriteLine(file.Name);
+                    //Console.WriteLine(file.Name);
                     continue;
                 }
             }
